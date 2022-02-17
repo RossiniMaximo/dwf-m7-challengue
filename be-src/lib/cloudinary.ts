668636ac30294c35abe9c94b78c2c0dded676276
Maximo <maximorossini2016@gmail.com>
2022-02-17 16:21:57 -0300

    eleventh commit

--- be-src/lib/cloudinary.ts
+++ be-src/lib/cloudinary.ts
@@ -2,7 +2,7 @@ const cloudinary = require("cloudinary").v2;
 cloudinary.config({
   cloud_name: "dqjrcykr7",
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: "poSSJ1j1MCm9u8iXdkHD1v3ebeo",
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export { cloudinary };
--- be-src/lib/seqConn.ts
+++ be-src/lib/seqConn.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize({
   dialect: "postgres",
   username: "pfyolxvytmtndv",
-  password: "4395e7509ee8702e8d71180be9fdc75f77b181501125b6ef93351036afc96653",
+  password: process.env.SEQUELIZE_PASS,
   database: "d6jknv4v56n0f8",
   port: 5432,
   host: "ec2-54-174-43-13.compute-1.amazonaws.com",
