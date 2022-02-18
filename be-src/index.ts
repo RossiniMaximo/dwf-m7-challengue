554163e4bcf053c10bd60c891d259c8c5f557655
Maximo <maximorossini2016@gmail.com>
2022-02-18 16:38:49 -0300

    last commit

--- be-src/index.ts
+++ be-src/index.ts
@@ -20,7 +20,7 @@ import { User } from "./models/user";
 
 const app = express();
 const port = process.env.PORT || 3001;
-app.use(express.json({ limit: "10 0mb" }));
+app.use(express.json({ limit: "100mb" }));
 app.use(cors());
 app.use(express.static("dist"));
 
