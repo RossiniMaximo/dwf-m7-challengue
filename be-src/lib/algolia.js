c7905c468385fcef311a1dd1cd3f6d9de3a59d78
Maximo <maximorossini2016@gmail.com>
2022-02-19 16:11:57 -0300

    last commit

--- be-src/lib/algolia.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.indexPets = void 0;
-var algoliasearch_1 = require("algoliasearch");
-require("dotenv").config();
-console.log("hola", process.env.ALGOLIA_API_KEY);
-var client = (0, algoliasearch_1["default"])(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_API_KEY);
-var indexPets = client.initIndex(process.env.ALGOLIA_INDEX_NAME);
-exports.indexPets = indexPets;
