57f186e1f229c1f66785cba20a5dafcb96de0e75
Maximo <maximorossini2016@gmail.com>
2022-02-21 17:28:06 -0300

    probando suerte

--- be-src/models/auth.ts
+++ be-src/models/auth.ts
@@ -1,6 +1,16 @@
 import { Model, DataTypes } from "sequelize";
 import { sequelize } from "../lib/seqConn";
 
+/* export class Auth extends Model {}
+Auth.init(
+  {
+    password: DataTypes.STRING,
+    email: DataTypes.STRING,
+    user_id: DataTypes.INTEGER,
+  },
+  { sequelize, modelName: "auth" }
+); */
+
 export const Auth = sequelize.define("auth", {
   password: DataTypes.STRING,
   email: DataTypes.STRING,
